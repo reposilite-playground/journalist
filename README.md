216d6ff88a944239f583d75f50767e678c1a042b
dzikoysk <dzikoysk@dzikoysk.net>
2021-08-31 01:43:29 +0200

    GH-17 Support Publisher based logger (Resolve #17)

--- README.md
+++ README.md
@@ -19,7 +19,7 @@ Available implementations:
     <artifactId>journalist</artifactId>
     <!-- For SL4J based implementations -->
     <artifactId>journalist-sl4j</artifactId>
-    <version>1.0.8</version>
+    <version>1.0.9</version>
 </dependency>
 ```
 Repository:
---
+++ journalist/src/main/java/com/reposilite/journalist/backend/PublisherLogger.java
@@ -0,0 +1,41 @@
+package com.reposilite.journalist.backend;
+
+import com.reposilite.journalist.Channel;
+import panda.std.Publisher;
+import panda.std.Subscriber;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import static com.reposilite.journalist.utils.EntryUtils.entryOf;
+
+public class PublisherLogger extends DefaultLogger implements Publisher<Integer, Entry<Channel, String>> {
+
+    private final Map<Integer, Subscriber<? super Entry<Channel, String>>> subscribers = new HashMap<>(2);
+    private int id = 0;
+
+    public PublisherLogger(Channel threshold) {
+        super(threshold);
+    }
+
+    @Override
+    protected void internalLog(Channel channel, String message) {
+        Entry<Channel, String> entry = entryOf(channel, message);
+
+        for (Subscriber<? super Entry<Channel, String>> subscriber : subscribers.values()) {
+            subscriber.onComplete(entry);
+        }
+    }
+
+    @Override
+    public Integer subscribe(Subscriber<? super Entry<Channel, String>> subscriber) {
+        int subscriberId = id++;
+        subscribers.put(subscriberId, subscriber);
+        return subscriberId;
+    }
+
+    public boolean unsubscribe(Integer subscriberId) {
+        return subscribers.remove(subscriberId) != null;
+    }
+
+}
